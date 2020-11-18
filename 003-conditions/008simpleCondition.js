438fb5390d24961a96fa4bae4844011763bd89b4
Emanuel Almeida <40006289+almemanuel@users.noreply.github.com>
2020-11-18 15:59:13 -0300

    conditions

---
+++ 003-conditions/008simpleCondition.js
@@ -0,0 +1,15 @@
+/* RM 11:36 */
+
+// velocity
+var vel = 60.5
+
+console.log(`Your car speed's ${vel} Km/h`)
+
+// simple condition
+if (vel > 60) {
+    console.log("You'll receive a traffic ticket because you've exceeded the speed limit")
+}
+
+console.log("Always wear your seat belt!")
+
+/* RM 11:36 */
---
+++ 003-conditions/009compoundCondition.js
@@ -0,0 +1,12 @@
+/* RM 11:36 */
+
+var country = 'USA'
+
+console.log(`Born in ${country}... `)
+if (country == 'Brazil') {
+    console.log("you're brazilian!")
+} else {
+    console.log("You're foreign!")
+}
+
+/* RM 11:36 */
