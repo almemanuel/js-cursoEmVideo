4bc07a23d0cf665266c8ef67a6479a68d98d8a9e
Emanuel Almeida <40006289+almemanuel@users.noreply.github.com>
2020-12-09 16:06:29 -0300

    On branch main 	new file:   004-loops/ambient.js

---
+++ 004-loops/ambient.js
@@ -0,0 +1,14 @@
+// RM 11:36
+
+var count = 1
+while (count <= 500) {
+    console.log(`Step ${count}`)
+    count++
+}
+
+do {
+    console.log(`Step ${count}`)
+    count--
+} while (count >= 0)
+
+// RM 11:36
