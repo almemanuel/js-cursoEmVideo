c99a69ca0a68702387b3276c34589098d9183c52
Emanuel Almeida <40006289+almemanuel@users.noreply.github.com>
2020-12-24 15:21:57 -0300

    On branch main 	renamed:    005-compoundVariables/ambient.js -> 005-compoundVariables/array.js 	new file:   006-functions/evenOrOdd.js

---
+++ 006-functions/evenOrOdd.js
@@ -0,0 +1,14 @@
+// RM 11: 36
+
+function evenOdd(number) {
+    if (number % 2 == 0) {
+        return `${number} is an EVEN number.`
+    } else {
+        return `${number} is an ODD number.`
+    }
+}
+
+
+console.log(evenOdd(4))
+
+// RM 11: 36
