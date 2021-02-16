31a12b339e62e805d9e63f10d095c936c8dfd8e2
Emanuel Almeida <40006289+almemanuel@users.noreply.github.com>
2021-02-16 16:04:22 -0300

    beta

--- exercises/007/script.js
+++ exercises/007/script.js
@@ -1,1 +1,28 @@
-alert('Hello')
+var num = document.querySelector('#number')
+var list = document.querySelector('#list')
+var res = document.querySelector('#res')
+var values = []
+
+function isNumber(n){
+    if (Number(n) >= 1 && Number(n) <= 100){
+        return true
+    } else {
+        return false
+    }
+}
+
+function inList(n, l) {
+    if (l.indexOf(Number(n)) != -1){
+        return true
+    } else {
+        return false
+    }
+}
+
+function add(){
+    if (isNumber(num.value) && !inList(num.value, values)){
+        window.alert('ok')
+    } else{
+        window.alert('Please, insert a number between 1 and 100.')
+    }
+}
