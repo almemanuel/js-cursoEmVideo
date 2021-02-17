20255023a21f620ae4fb6684c91f23b7d0d8d913
Emanuel Almeida <40006289+almemanuel@users.noreply.github.com>
2021-02-17 11:55:38 -0300

    On branch main 	modified:   exercises/007/index.html 	modified:   exercises/007/script.js 	modified:   exercises/007/style.css

--- exercises/007/script.js
+++ exercises/007/script.js
@@ -1,7 +1,7 @@
 var num = document.querySelector('#number')
 var list = document.querySelector('#list')
 var res = document.querySelector('#res')
-var values = []
+var userEnter = []
 
 function isNumber(n){
     if (Number(n) >= 1 && Number(n) <= 100){
@@ -20,9 +20,47 @@ function inList(n, l) {
 }
 
 function add(){
-    if (isNumber(num.value) && !inList(num.value, values)){
-        window.alert('ok')
-    } else{
-        window.alert('Please, insert a number between 1 and 100.')
+    if (isNumber(num.value) && !inList(num.value, userEnter)){
+        userEnter.push(Number(num.value))
+        let item = document.createElement('option')
+        item.text = `${num.value} add to list!`
+        list.appendChild(item)
+        res.innerHTML = ''
+    } else if (!isNumber(num.value)){
+        alert('Please, insert a valid number.')
+    } else if (inList(num.value, userEnter)){
+        alert('This number is already on the list. Please enter another one bewteen 1 and 100.')
+    }
+
+    num.value = ''
+    num.focus()
+}
+
+function finish() {
+    if (userEnter.length == 0) {
+        alert('Unable to finish without userEnter.')
+    } else {
+        let size = userEnter.length
+        let greater = userEnter[0]
+        let less = userEnter[0]
+        let sum = 0
+        let average = 0
+
+        for(let i in userEnter) {
+            sum += userEnter[i]
+            if (userEnter[i] > greater) {
+                greater = userEnter[i]
+            } else if (userEnter[i] < less) {
+                less = userEnter[i]
+            }
+        }
+
+        average = sum / size
+
+        res.innerHTML = ''
+        res.innerHTML += `<p>We have ${size} numbers registred</p>`
+        res.innerHTML += `<p>The largest number reported wax ${greater}, and the smallest, ${less}</p>`
+        res.innerHTML += `<p>All summed values result in </p>`
+        res.innerHTML += `<p>The average is ${average.toFixed(2)}</p>`
     }
 }
